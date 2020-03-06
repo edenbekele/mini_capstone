c2fcf11999c4609300cb53677b3a7d44ee8680ac
EDEN <EDBEK@YAHOO.COM>
2020-03-05 20:33:53 -0600

    Added update and destroy method

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -9,6 +9,7 @@ class Api::ProductsController < ApplicationController
       name: params["name"],
       price: params["price"],
       description: params["description"],
+      image_url: params["image_url"],
     )
     @product.save
     render "show.json.jb"
@@ -17,5 +18,23 @@ class Api::ProductsController < ApplicationController
   def show
     @product = Product.find_by(id: params["id"])
     render "show.json.jb"
+  end
+
+  def update
+    @product = Product.find_by(id: params["id"])
+
+    @product.id = params["id"] || @product.id
+    @product.name = params["name"] || @product.name
+    @product.price = params["price"] || @product.price
+    @product.description = params["description"] || @product.description
+    @product.image_url = params["iamge_url"] || @product.image_url
+    @product.save
+    render "show.json.jb"
+  end
+
+  def destroy
+    @product = Product.find_by(id: params["id"])
+    @product.destroy
+    render json: { message: "Product successfully destroyed!" }
   end
 end
