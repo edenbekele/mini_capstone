ce30aebf995d817846417345e19310475e56d97f
EDEN <EDBEK@YAHOO.COM>
2020-03-09 21:24:05 -0500

    Added discount?, tax, and total. Added validations

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -11,8 +11,11 @@ class Api::ProductsController < ApplicationController
       description: params["description"],
       image_url: params["image_url"],
     )
-    @product.save
-    render "show.json.jb"
+    if @product.save
+      render "show.json.jb"
+    else
+      render json: { errors: @product.errors.full_messages }
+    end
   end
 
   def show
@@ -27,9 +30,12 @@ class Api::ProductsController < ApplicationController
     @product.name = params["name"] || @product.name
     @product.price = params["price"] || @product.price
     @product.description = params["description"] || @product.description
-    @product.image_url = params["iamge_url"] || @product.image_url
-    @product.save
-    render "show.json.jb"
+    @product.image_url = params["image_url"] || @product.image_url
+    if @product.save
+      render "show.json.jb"
+    else
+      render json: { errors: @product.errors.full_messages }
+    end
   end
 
   def destroy
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,18 @@
 class Product < ApplicationRecord
+  validates :name, presence: true
+  validates :name, uniqueness: true
+  validates :price, presence: true
+  validates :price, numericality: { greater_than: 0 }
+
+  def is_discounted?
+    price < 5
+  end
+
+  def tax
+    price * 0.09
+  end
+
+  def total
+    price + tax
+  end
 end
